3c2d019dd76f065b0407304dc92876d5a444e8d7
osnat ben ari <osnat122ster@gmail.com>
2020-10-12 11:45:12 +0300

    2

--- src/App.js
+++ src/App.js
@@ -21,15 +21,15 @@ class App extends React.Component {
 
     }
     
-    getHashParams() {
-        let hashParams = {};
-        let e, r = /([^&;=]+)=?([^&;]*)/g,
-            q = window.location.hash.substring(1);
-        while ( e = r.exec(q)) {
-           hashParams[e[1]] = decodeURIComponent(e[2]);
-        }
-        return hashParams;
-    }
+    // getHashParams() {
+    //     let hashParams = {};
+    //     let e, r = /([^&;=]+)=?([^&;]*)/g,
+    //         q = window.location.hash.substring(1);
+    //     while ( e = r.exec(q)) {
+    //        hashParams[e[1]] = decodeURIComponent(e[2]);
+    //     }
+    //     return hashParams;
+    // }
 
     render() { 
         return ( 
